7571959bce8fef0c96cc2dd9d30f4c9dedeb5c0c
Ali Senol <mrmeerkaat@gmail.com>
2024-11-16 17:08:37 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,11 +1,8 @@
-# import dataTransform
-import MAGA
 import data_transform
 import numpy as np
 
 import matplotlib.pyplot as plt
 from matrix_test import random_matrix
-from ogData_streaming import data_prepare, data_stream, data_cleanup, stream_length
 from data_streaming import data_prepare, data_stream, data_cleanup, stream_length
 
 def plot_frequency_bands(filtered_data):
@@ -33,7 +30,6 @@ def main():
 
     while timeElapsed < totalTime:
         someData = data_stream(board, stream_length)
-        filtered_data = MAGA.data_transform(someData)
         filtered_data = data_transform.data_transform(someData)
 
         for key in filtered_data:
